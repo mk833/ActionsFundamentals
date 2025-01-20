dbc117ef0253a0b8d9f0bd816321e9a969403bf6
mk833 <122925911+mk833@users.noreply.github.com>
2025-01-20 15:51:04 -0600

    Update entrypoint.sh

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -1,2 +1,12 @@
+#!/bin/sh -l
+
+# Use INPUT_<INPUT_NAME> to get the value of an input
+GREETING="Hello, $INPUT_WHO_TO_GREET!"
 echo "hello $1"
-echo "time=$(date) >> $GITHUB_OUTPUT
+# Use workflow commands to do things like set debug messages
+echo "::notice file=entrypoint.sh,line=7::$GREETING"
+
+# Write outputs to the $GITHUB_OUTPUT file
+echo "time=$(date)" >>"$GITHUB_OUTPUT"
+
+exit 0
